af80f75033d9c9c1323e39a8655676e60cfb28cd
padilha <padilha@efad012e-8612-41d9-818e-9d1cb86e76e3>
2010-05-13 16:54:30 +0000

    Alterando servidor de banco

--- class/Dominio.php
+++ class/Dominio.php
@@ -11,7 +11,7 @@ class Dominio
 	public $DB_HOST = "mysql01.joaopadilha.com"; //mysql01.servicodespertador.net 
 	public $DB_USER = "servicodespert"; 
 	public $DB_PASSWD = "Mend2009"; 
-	public $DB_DATA = "servicodespert";
+	public $DB_DATA = "servicodespertador";
 	
 	public $path = "http://localhost/SMC/www/new";    
 	public $path_interno = "C:/Paginas_Sistemas/SMC/www/new";
