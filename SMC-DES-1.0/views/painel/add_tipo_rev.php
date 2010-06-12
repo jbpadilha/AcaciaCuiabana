d680b471553087e3511c4204e78e4ad80cdd6499
padilha <padilha@efad012e-8612-41d9-818e-9d1cb86e76e3>
2010-06-12 15:50:56 +0000

    Geração da Versão 1.1

---
+++ SMC-DES-1.0/views/painel/add_tipo_rev.php
@@ -0,0 +1,56 @@
+<?php
+require_once ('../../class/Config.php');
+
+if(!isset($_SESSION['usuarioLogon'])) {
+    header("Location:../views/home.php?p=login");
+}
+
+$logon = new Logon();
+$logon = $_SESSION["usuarioLogon"];
+var_dump($logon);
+if($logon->getNivelAcessoLogin() != Dominio::$ADMINISTRADOR) {
+    $mensagem = 'Usuário não autorizado para a funcionalidade';
+    echo "
+    <script type=\"text/javascript\">
+		document.location='index.php?pg=home&msg=$mensagem';
+	</script>";
+}
+
+$tipoRevisoes = new Tiporevisoes();
+
+if(isset($_GET['tipoRevisoes'])) {
+    $tipoRevisoes = unserialize(base64_decode($_GET['tipoRevisoes']));
+}
+?>
+<form method="post" action="../../class/RecebePostGet.php" >
+    <p class="caption"> Novo item de revisão </p>
+
+    <fieldset>
+        <label>Descrição:
+            <input type="text" class="x9" name="descricao" id="descricao" value="<?=$tipoRevisoes->getDescricaoTipoRevisoes()?>" />
+        </label>
+    </fieldset>
+
+    <p class="botoes">
+        <input type="hidden" name="acao" value="cadastroTipoRevisoes" />
+        <input type="submit" value="Confirmar" />
+    </p>
+</form>
+
+<form method="post" action="../../class/RecebePostGet.php" >
+    <p class="caption">Lista de revisões já cadastradas</p>
+
+    <fieldset>
+        <?php
+        $pesquisaTipoRevisoes = new Tiporevisoes();
+        $collVoTipoRevisoes = $controla->findTipoRevisoes($pesquisaTipoRevisoes);
+        if(!is_null($collVoTipoRevisoes)) {
+            foreach ($collVoTipoRevisoes as $tipoRevisao) {
+                $tipoRevisoesAtual = new Tiporevisoes();
+                $tipoRevisoesAtual = $tipoRevisao;
+                echo "<b>{$tipoRevisoesAtual->getDescricaoTipoRevisoes()}</b><br>";
+            }
+        }
+        ?>
+    </fieldset>
+</form>
