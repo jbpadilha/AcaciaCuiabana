2b578126e310f3438bfe24ec0faaea2be879e409
padilha <padilha@efad012e-8612-41d9-818e-9d1cb86e76e3>
2010-12-09 00:37:36 +0000

    Papapizza

--- Papapizza/oscommerce2/loja/includes/languages/portugues/modules/payment/pagseguro.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('MODULE_PAYMENT_PAGSEGURO_FORM_ACTION_URL', 'https://pagseguro.uol.com.br/security/webpagamentos/webpagto.aspx');
-define('MODULE_PAYMENT_PAGSEGURO_TEXT_TITLE', 'Modulo PagSeguro');
-define('MODULE_PAYMENT_PAGSEGURO_TEXT_PUBLIC_TITLE', 'PagSeguro');
-define('MODULE_PAYMENT_PAGSEGURO_TEXT_DESCRIPTION', 'Modulo que implementa todas as funcoes necessarias para voce utilizar o meio de pagamento PagSeguro');
-?>
