0dc091185340c763c697c84b63cd532fdd449cfd
padilha <padilha@efad012e-8612-41d9-818e-9d1cb86e76e3>
2011-01-18 06:05:55 +0000

    Atualização - Correção XML

--- Papapizza/oscommerce/admin/includes/languages/en_BR/banner_manager.php
+++ Papapizza/oscommerce/admin/includes/languages/en_BR/banner_manager.php
@@ -17,24 +17,24 @@ action_heading_new_banner = Novo Banner
 action_heading_batch_delete_banners = Remover Grupo de Banners
 
 operation_heading_type = Tipo :
-operation_heading_month = M√™s :
+operation_heading_month = MÍs :
 operation_heading_year = Ano :
 
 table_heading_banners = Banners
 table_heading_group = Grupo
-table_heading_statistics = Estat√≠sticas
-table_heading_action = A√ß√£o
+table_heading_statistics = EstatÌsticas
+table_heading_action = AÁ„o
 table_heading_source = Fonte
 table_heading_views = Ver
 table_heading_clicks = Cliques
 
-section_daily = Di√°rio
+section_daily = Di·rio
 section_monthly = Mensal
 section_yearly = Anual
 
-subsection_heading_statistics_daily = %s Estat√≠sticas di√°rias para %s %s
-subsection_heading_statistics_monthly = %s Estat√≠sticas Mensais para %s
-subsection_heading_statistics_yearly = %s Estat√≠sticas Anuais
+subsection_heading_statistics_daily = %s EstatÌsticas di·rias para %s %s
+subsection_heading_statistics_monthly = %s EstatÌsticas Mensais para %s
+subsection_heading_statistics_yearly = %s EstatÌsticas Anuais
 
 field_title = Titulo :
 field_url = URL:
@@ -46,22 +46,22 @@ field_image_target = Image Target (Salvar para):
 field_html_text = HTML Texto:
 field_scheduled_date = Data Programada :
 field_expiry_date = Data que expira :
-field_maximum_impressions = Impress√µes M√°ximas :
+field_maximum_impressions = Impressıes M·ximas :
 field_status = Status:
 field_delete_image = Deletar Banner ?
 
-introduction_new_banner = Preencha a seguinte informa√ß√£o para o Novo Banner .
+introduction_new_banner = Preencha a seguinte informaÁ„o para o Novo Banner .
 
-introduction_edit_banner = Fa√ßa as mudan√ßas necess√°rias para este banner .
+introduction_edit_banner = FaÁa as mudanÁas necess·rias para este banner .
 
-introduction_delete_banner = Verifique a remo√ß√£o deste Banner .
+introduction_delete_banner = Verifique a remoÁ„o deste Banner .
 
-introduction_batch_delete_banners = Verifique a remo√ß√£o dos seguintes banners .
+introduction_batch_delete_banners = Verifique a remoÁ„o dos seguintes banners .
 
-info_banner_fields = <b>Notas do Banner :</b><ul><li>Use uma imagem ou texto para o Banner - n√£o ambos em HTML.</li><li>O texto em HTML tem prioridade sobre a Imagem</li></ul>
-<b>Notas da Imagem :</b><ul><li>Os arquivos de recebimento (upload) das Imagens tem que ter as permiss√µes apropriadas para escrita !</li><li>N√£o salve os arquivos de transferencia se voce n√£o est√° transferindo (uploading) as imagens para o servidor web ou se voce est√° usando uma imagem local.</li><li>O Salvamento tem que ser em um diret√≥rio existente espec√≠fico terminado com uma barra(exemplo : banners/).</li></ul>
-<b>Notas de Expira√ß√£o :</b><ul><li>Somente um dos dois campos deve ser preenchido</li><li>Se o Banner n√£o tiver tempo para expirar autom√°ticamente, deixe um espa√ßo em branco dentro desses campos</li></ul>
-<b>Programa√ß√£o :</b><ul><li>Se uma programa√ß√£o √© ajustada, o banner tem estar ativo nesta data.</li><li>Todos os banners programados est√£o marcados como desativados at√° que sua data chegue, o que entÔøΩo ser√£o ativados</li></ul>
+info_banner_fields = <b>Notas do Banner :</b><ul><li>Use uma imagem ou texto para o Banner - n„o ambos em HTML.</li><li>O texto em HTML tem prioridade sobre a Imagem</li></ul>
+<b>Notas da Imagem :</b><ul><li>Os arquivos de recebimento (upload) das Imagens tem que ter as permissıes apropriadas para escrita !</li><li>N„o salve os arquivos de transferencia se voce n„o est· transferindo (uploading) as imagens para o servidor web ou se voce est· usando uma imagem local.</li><li>O Salvamento tem que ser em um diretÛrio existente especÌfico terminado com uma barra(exemplo : banners/).</li></ul>
+<b>Notas de ExpiraÁ„o :</b><ul><li>SÛmente um dos dois campos deve ser preenchido</li><li>Se o Banner n„o tiver tempo para expirar autom·ticamente, deixe um espaÁo em branco dentro desses campos</li></ul>
+<b>ProgramaÁ„o :</b><ul><li>Se uma programaÁ„o È ajustada, o banner tem estar ativo nesta data.</li><li>Todos os banners programados est„o marcados como desativados atÈ que sua data chegue, o que ent„o ser„o ativados</li></ul>
 
-ms_error_graphs_directory_non_existant = Erro : O diret√≥rio dos gr√°ficos n√£o existe : %s
-ms_error_graphs_directory_not_writable = Erro : O diret√≥rio dos gr√°ficos n√£o √© escritur√°vel, ou n√£o tem permissÔøΩes de escrita: %s
+ms_error_graphs_directory_non_existant = Erro : O diretÛrio dos gr·ficos n„o existe : %s
+ms_error_graphs_directory_not_writable = Erro : O diretÛrio dos gr·ficos n„o È escritur·vel, ou n„o tem permissıes de escrita: %s
--- Papapizza/oscommerce/admin/includes/languages/en_BR/categories.php
+++ Papapizza/oscommerce/admin/includes/languages/en_BR/categories.php
@@ -16,7 +16,7 @@ heading_title = Categorias
 operation_title_search = Busca :
 
 table_heading_categories = Categorias
-table_heading_action = A√ß√£o
+table_heading_action = AÁ„o
 
 action_heading_new_category = Nova Categoria
 action_heading_batch_delete_categories = Remover Grupo de Categorias
@@ -29,21 +29,21 @@ field_sort_order = Ordem :
 
 top_category = -- Top --
 
-introduction_new_category = Preencha a seguinte informa√ß√£o para a nova Categoria .
+introduction_new_category = Preencha a seguinte informaÁ„o para a nova Categoria .
 
-introduction_edit_category = Fa√ßa as mudan√ßas necess√°rias para esta Categoria .
+introduction_edit_category = FaÁa as mudanÁas necess·rias para esta Categoria .
 
 introduction_move_category = Selecione a Categoria Principal para que possa ser movida .
 
-introduction_delete_category = Verifique a remo√ß√£o desta categoria e de suas sub-categorias .
-delete_warning_category_in_use_children = Aviso : H√° sub-categorias de %s que ser√£o removidas com ela. Voce tem certeza de que √© isso que quer fazer mesmo?
-delete_warning_category_in_use_products = Aviso : Existem Produtos de %s que ter√£o suas rela√ß√µes com ela removidas juntamente com a categoria .
+introduction_delete_category = Verifique a remoÁ„o desta categoria e de suas sub-categorias .
+delete_warning_category_in_use_children = Aviso : H· sub-categorias de %s que ser„o removidas com ela. Voce tem certeza de que È isso que quer fazer mesmo?
+delete_warning_category_in_use_products = Aviso : Existgm Produtos de %s que ter„o suas relaÁıes com ela removidas juntamente com a categoria .
 
-introduction_batch_delete_categories = Verifique a remo√ß√£o das seguintees categorias e de suas sub-categorias .
+introduction_batch_delete_categories = Verifique a remoÁ„o das seguintees categorias e de suas sub-categorias .
 
 introduction_batch_move_categories = Selecione qual categoria principal as seguintes categorias devem ser movidas .
 
-ms_warning_category_name_empty = Aviso : O nome da categoria de %s est√° sem preenchimento. Preencha um Nome para esta Categoria .
+ms_warning_category_name_empty = Aviso : O nome da categoria de %s est· sem preenchimento. Preencha um Nome para esta Categoria .
 
-ms_error_image_directory_not_writable = Erro : O diret√≥rio que cont√©m as Imagens da Categoria n√£o est√° escritur√°vel : %s
-ms_error_image_directory_non_existant = Erro : O arquivo para armazenar as Imagens da Categoria n√£o existe : %s
+ms_error_image_directory_not_writable = Erro : O diretÛrio que contÈm as Imagens da Categoria n„o est· escritur·vel : %s
+ms_error_image_directory_non_existant = Erro : O arquivo para armazenar as Imagens da Categoria n„o existe : %s
